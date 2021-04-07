6de18a7a8b01891d3c8cbd71fe943b9cf206d8c3
LordAldi <aldianugra09@gmail.com>
2021-04-07 22:28:28 +0700

    finish iconText

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import IconText from "components/IconText";
+import { BrowserRouter } from "react-router-dom";
 import "./assets/scss/style.scss";
 
 function App() {
-  return <div className="App">haii</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <IconText />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/IconText/index.js
@@ -0,0 +1,9 @@
+import Button from "components/Button";
+
+export default function IconText(props) {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Stay<span className="text-gray-900">Cation.</span>
+    </Button>
+  );
+}
